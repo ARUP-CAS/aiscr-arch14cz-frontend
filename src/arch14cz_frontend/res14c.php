ac81531749dc946994575f3b28bf2a9e14387613
demjanp <peter.demjan@gmail.com>
2024-06-20 14:08:32 +0200

    updated Res14C instructions; updated vocabularies in arch14CZ_import_sample_contribute.xlsx;

--- src/arch14cz_frontend/res14c.php
+++ src/arch14cz_frontend/res14c.php
@@ -28,6 +28,26 @@
 	</div>
 	<button id="exportButton" style="display: none;">Download Data (CSV)</button>
 </div>
+
+<!-- Explanation Text Start -->
+<h3>Example Use:</h3>
+<p>Let's say we want to know the expected dating resolution for samples with an actual age around 2000 BC. Here's how we can do it:</p>
+<ol>
+    <li><strong>Enter Age Range:</strong> In the "Enter sample age" section, input -2100 as the minimum value and -1900 as the maximum value. These correspond to 2100 BC and 1900 BC.</li>
+    <li><strong>Plot the Data:</strong> Click the "Plot" button.</li>
+</ol>
+<h3>Understanding the Results:</h3>
+<ul>
+    <li>The resulting graph will show the resolution of radiocarbon dating for this age range.</li>
+    <li>The grey shaded area represents the 90% confidence interval, indicating the range within which we can expect most of our dates to fall.</li>
+    <li>The black line represents the mean resolution.</li>
+</ul>
+<h3>Interpreting the Data:</h3>
+<p>For the given age range (2100-1900 BC), the graph shows that the dating resolutions vary between approximately 130 and 230 years.</p>
+<p>This means that if we were to date 20 samples from this period, we can expect 19 of them to have a dating uncertainty range (2-sigma or 95.45% confidence interval) between 130 and 230 years.</p>
+<p>In simpler terms, when we date samples from 2000 BC, the results will generally be accurate within a range of 130 to 230 years. This helps us understand how precise our dating is for this specific period.</p>
+<!-- Explanation Text End -->
+
 <p>Project home: <a href="https://github.com/demjanp/res14c_online">https://github.com/demjanp/res14c_online</a> (Python script used to generate the data as well as the full open source code).</p>
 <p>For an overview of the method see: Svetlik et al. (2019) <a href="https://doi.org/10.1017/RDC.2019.134">DOI: 10.1017/RDC.2019.134</a>.</p>
 <p>The expected measurement uncertainty for 0 BP is ±15 radiocarbon years. This value grows exponentially depending on the age of the sample (see <a href="https://doi.org/10.1017/RDC.2019.134">Svetlik et al. 2019</a> for the exact formula).</p>
